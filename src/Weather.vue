0b8735e6b2dfb8898297906a10c2c482dae446ec
larsvo@involvit.nl <larsvo@involvit.nl>
2025-03-21 10:04:20 +0100

    rework

--- src/Weather.vue
+++ src/Weather.vue
@@ -1,36 +1,56 @@
 <script setup>
 import {computed} from 'vue';
-
 </script>
 <template>
-  <div class="weather" id="Wapp">
+  <div class="weather" id="Weather">
     <section class="weather__form">
-      <H1 class="weather__title">Forecast Friend</H1>
       <form @submit.prevent="getWeather">
+        <label class="weather__title" for="zoekveld">Forecast Friend</label>
+        <div class="weather__input">
         <input type="text"
-               placeholder="Zoek op locatie"
+               id="zoekveld"
+               placeholder="Zoek op locatie bijvoorbeeld Amsterdam"
                v-model="query"
                class="weather__search"
         >
-        <button type="submit">Zoeken</button>
-        <div class="weather__widget" v-if="typeof weather.main != 'undefined'">
-          <WeatherIcon :condition="weather.weather[0].main" :key="weather.weather[0].main"/>
-          <div class="weather__body">
-            <div class="weather__conditions">
-              <div class="weather__temperature">
-                {{ Math.round(weather.main.temp) }}&deg;c
-              </div>
-              <div class="weather__description">
-                {{ translateWeather(weather.weather[0].main) }}
-              </div>
-              <div class="weather__location">
-                <h2 class="weather__city">{{ weather.name }}, {{ weather.sys.country }}</h2>
-                <p class="weather__date">{{ currentDate }}</p>
-              </div>
+        <button type="submit" aria-label="Zoeken">
+          <svg width="24px" height="24px" viewBox="0 0 35 35">
+            <title>search icon</title>
+            <g id="Page-5" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+              <g id="001_Homepage" transform="translate(-871.000000, -778.000000)" stroke="var(--main-color)" stroke-width="5">
+                <g id="Group-7" transform="translate(194.000000, 748.000000)">
+                  <g id="Group-6" transform="translate(656.000000, 10.000000)">
+                    <g id="Group-5" transform="translate(21.000000, 20.000000)">
+                      <circle id="Oval" cx="16" cy="16" r="13.5"></circle>
+                      <line x1="26.5" y1="26.5" x2="32.5" y2="32.5" id="Line" stroke-linecap="round"></line>
+                    </g>
+                  </g>
+                </g>
+              </g>
+            </g>
+          </svg>
+        </button>
+        </div>
+      </form>
+      <p v-if="errorMessage" class="weather__city">{{ errorMessage }}</p>
+      <div class="weather__widget" v-if="typeof weather.main != 'undefined'">
+        <WeatherIcon :condition="weather.weather[0].main" :key="weather.weather[0].main"/>
+        <div class="weather__body">
+          <div class="weather__conditions">
+            <div class="weather__temperature">
+              {{ Math.round(weather.main.temp) }}&deg;c
+            </div>
+            <div class="weather__description">
+              {{ translateWeather(weather.weather[0].main) }}
+            </div>
+            <div class="weather__location">
+              <h2 class="weather__city">{{ weather.name }}, {{ weather.sys.country }}</h2>
+              {{ this.errorMessage }}
+              <p class="weather__date">{{ currentDate }}</p>
             </div>
           </div>
         </div>
-      </form>
+      </div>
     </section>
   </div>
 </template>
@@ -45,7 +65,7 @@ export default {
   components: {
     WeatherIcon,
   },
-  name: 'Wapp',
+  name: 'Weather',
   data() {
     return {
       api_key: '723ecbe430b06b6b28e464fbb099215e',
@@ -73,11 +93,10 @@ export default {
         this.errorMessage = ''; // Reset eventuele eerdere foutmeldingen
       } catch (error) {
         console.error('Error fetching weather data:', error);
-        this.errorMessage = 'Fout bij het ophalen van weergegevens';
+        this.errorMessage = 'Geen weer gegevens gevonden';
         this.weather = {}; // Reset weerdata bij een fout
       }
     },
-
     translateWeather(englishTerm) {
       const translations = {
         'Clear': 'Helder',
