7412ccf5347b8843bb0ccb54be659ebb0299c897
larsvo@involvit.nl <larsvo@involvit.nl>
2025-03-21 12:17:20 +0100

    looking nice

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,28 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { fileURLToPath, URL } from "url";
+import {fileURLToPath, URL} from "url";
 import svgLoader from "vite-svg-loader";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-      vue(),
-    svgLoader(),
-  ],
-  css: {
-    preprocessorOptions: {
-      scss: {
-       
-      }
-    }
-  },
-  build: {
-    cssCodeSplit: false
-  },
-  server: {
-    host: true // or '0.0.0.0'
-  },
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    plugins: [
+        vue(),
+        svgLoader(),
+    ],
+    css: {
+        preprocessorOptions: {
+            scss: {}
+        }
     },
-  },
+    build: {
+        cssCodeSplit: false
+    },
+    server: {
+        host: true // or '0.0.0.0'
+    },
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
+    },
 })
